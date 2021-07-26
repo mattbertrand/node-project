2f775d31b0ffc205afcd78a3561f58c3802a3809
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-26 16:52:02 -0700

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://mattbertrand:mattbertrand@cluster0.unqt7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useUnifiedTopology: true },
+() => console.log('connected to db!')
+)
 
 const authRoute = require('./routes/auth')
 
