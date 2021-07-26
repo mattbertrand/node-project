138050c914bc6f0422a030feb8943d345b6e395d
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-26 16:57:43 -0700

    added dotenv to protect informations

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://mattbertrand:mattbertrand@cluster0.unqt7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{ useUnifiedTopology: true },
-() => console.log('connected to db!')
+dotenv.config()
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useUnifiedTopology: true },
+    () => console.log('connected to db!')
 )
 
 const authRoute = require('./routes/auth')
