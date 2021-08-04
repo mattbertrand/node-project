0770751612c6ea4ae6c6c78d892492ed52c96503
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-04 11:57:30 -0700

    protected the data sent

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.post('/register', async (req, res) => {
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser)
+        res.send({user: user._id})
     } catch(err){
         res.status(400).send(err);
     }
